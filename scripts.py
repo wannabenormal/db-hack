d0c5db389501d05b87d166b02cd86a68bd983a64
Alexey Pesotsky <pesthedog@mail.ru>
2022-02-02 20:56:53 +0400

    Add function for removing chastisements

--- scripts.py
+++ scripts.py
@@ -6,3 +6,6 @@ from datacenter.models import Schoolkid, Lesson, Commendation
 def fix_marks(schoolkid):
     schoolkid.mark_set.filter(points__in=[2, 3]).update(points=5)
 
+
+def remove_chastisements(schoolkid):
+    schoolkid.chastisement_set.all().delete()
